f7f16693d9a3fc64d8c13f6da6f8f4d23af88b4a
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-09-30 15:54:58 +0200

    Add project files.

---
+++ TextMatch/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TextMatch
+{
+    internal class Program
+    {
+        public static void main(string[] args)
+        {
+            if (args.Length > 2 || args.Length < 2)
+            {
+                Console.WriteLine("Invalid number of input parameters");
+                return;
+            }
+
+            string text = args[0];
+            string subtext = args[1];
+        }
+    }
+}
