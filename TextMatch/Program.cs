1d477d73e1fab12b5143382d12b938fb4d81b76b
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-09-30 16:05:36 +0200

    Setup preliminary Program.cs

--- TextMatch/Program.cs
+++ TextMatch/Program.cs
@@ -12,6 +12,21 @@
 
             string text = args[0];
             string subtext = args[1];
+
+            TextMatcher match = new TextMatcher(text);
+
+            List<int> matchedResults = match.Match(subtext);
+            if (matchedResults.Count > 0)
+            {
+                Console.WriteLine(PrintIndexes(matchedResults).ToString());
+            }
+        }
+
+        private static string PrintIndexes(List<int> indexes) 
+        {
+            string ReturnString = "";
+
+            return ReturnString;
         }
     }
 }
